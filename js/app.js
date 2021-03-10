e70734b147c0b43cd73e26d4d35dbea2c46151c5
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-11 00:27:09 +0200

    ready

--- js/app.js
+++ js/app.js
@@ -1,153 +1,139 @@
 'use strict';
-let  salesHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+let  salesHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-let  allcookieshops = [];
-//Get cookie sold to table 
-let  cookieshopTable = document.getElementById('cookies-sold');
-
-let  cookieShopshape = document.getElementById('add-shop-shape');
-let custPerHr
-=======
-let  alllocationshops = [];
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
-=======
-let  alllocationshops = [];
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
+let  alllocationshops = [seatle,lima,paris,dubai,tokyo];
 function CookieShop(location, minCust, maxCust, cookiesPerSale) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.cookiesPerSale = cookiesPerSale;
   this.cookiesSoldPerHr = [];
-  alllocationshops.push(this);
-
+  this.hourlycustemer=[];
+  this.avgsold= avgsold;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
+  CookieShop.prototype.hourlycustemer=function(){
+    console.log(this.location +' ' +this.cookiesPerSale+'is the best today');
+    let cookiesSoldPerHr=this.total;
+    for (let i=0;i<salesHour.length;i++){
+       this.hourlycustemer.push(random(this.maxCust,this.minCust));
+    }
 
-debugger;
-CookieShop.Initiallook.custPerHr = function () {
-  return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust))+ this.minCust);
-};
-
-CookieShop.Initiallook.cookiesPerHr = function () {
-  return Math.round(this.cookiesPerSale * this.custPerHr());
-};
-
-CookieShop.Initiallook.render = function() { 
-  let tablerow = document.createElement('tr');
-  let tablecolumn = document.createElement('th');
-  tablerow.innerText = this.location;
-  tablecolumn.appendChild(tablecolumn);
-
-  let sold = 0;
-  let finalSoldcounter = 0;
-
-  for (let i = 0; i < salesHour.length; i++) {
-
-    cookiesSold = this.cookiesPerHr();
-
-    let tablerow= document.createElement('td');
-    tablerow.innertext = cookiesSold;
-    tablerow.appendChild(tablecolumn);
-
-    this.cookiesSoldPerHr.push(Sold);
-
-    finalSoldcounter += Sold;
+  }
+  let soldcookies = 0; 
+let totalCookiesSold = 0;
+  CookieShop.prototype.cookiesSoldPerHr=function(){
+    this.hourlycustemer();
+    for (let i=0;i<salesHour.length;i++){
+      let hour=math.ceil(this.hourlycustemer[i]*this.avgsold);
+      let soldcookies = this.cookiesSoldPerHour();
+      location[i] = soldcookies;
+      totalCookiesPerDay = totalCookiesPerDay + soldcookies;
+    }
+    this.totalCookiesSoldInADay = totalCookiesPerDay;
+    location[i] = this.totalCookiesSoldInADay;
+    return location;
   }
 
-  tablecolumn = document.createElement('td');
-  tablecolumn.innertext =finalSoldcounter ;
-  tablerow.appendChild(tdElement);
-  cookieshopTable.appendChild(tablerow);
-};
+  let  newShop=document.getElementById('newshop');
+  
+  function handleshop(event){
+    event.preventDefault();
+    newShop.addEventListener('submit', handleshop);
+    let newShop =event.target.place.value;
+    let  max=parseInt(event.target.max.value);
+    let  min=parseInt(event.target.min.value);
+    let avgsold=parseInt(event.target.avgsold.value);
+    if( totalCookiesSoldInADay===mincus){
+      alert('this a bad day sales');
+    }
+    else{
+  
+      let addnewlocatin= new Location(newShop, mincus, avgsold, maxcus);
+      location.push(addnewlocatin);
+      totalcookiesSoldPerHour = 0;
+      addnewlocatin.hourlycustemer();
+      addnewlocatin.cookiesSoldPerHour();
+      alllocationshops();
+      console.log('total ' + totalcookiesSoldPerHour);
+    }
+  }
+  
+    let table = document.getElementById('cookietable');
+    let table = document.createElement('table');
+    let trow = [];
+  
+  CookieShop.prototype.render=function(){
+    this.cookiesSoldPerHr();
+    let trow=document.createElement('tr');
+    let td=document.createElement('td');
+    td.innerText=this.location;
+    trow.appendChild(td);
 
-function makeHeaderRow() { 
- let headElement = document.createElement('head');
-  let  tablerow = document.createElement('tr');
-  let  tablecolumn = document.createElement('th');
-  thElement.innertext = '';
-  tablerow.appendChild(tablecolumn);
 
-  for (let  i = 0; i < saleHour.length; i++) {
-    tablecolumn = document.createElement('th');
-    tablecolumn.innerText = saleHour[i];
-    tablerow.appendChild(tablecolumn);
+    for (let i=0;i<salesHour.length;i++){
+       td=document.createElement('td');
+       td.innerText=this.cookiessoldperhour[i];
+       trow.appendChild(td);
+
+  }
+  let theader=document.createElement('th');
+  theader.innerText=this.totalCookiesSold;
+  trow.appendChild(th);
+  table.appendChild(trow);
   }
 
-  tablecolumn = document.createElement('th');
-  tablecolumn.innerText = 'Daily sum';
-  tablerow.appendChild(tablecolumn);
-  headElement.appendChild(tablerow);
-
-  cookieshopTable.appendChild(headElement);
-=======
-=======
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
 let  firstshop=new  CookieShop('seatel', 23, 65, 6.3);
 let  secondshop=new  CookieShop('lima', 2, 16, 4.6);
 let  thirdshop=new   CookieShop('paris', 20, 38,2.3 );
 let  forthshop=new  CookieShop('dubai', 11, 38, 3.7);
 let  fifthshop=new  CookieShop('tokyo', 3, 24, 1.2);
-CookieShop.prototype.bestsalestoday=function(){
-  console.log(this.location +' ' +this.cookiesPerSale+'is the best today');
-}
-console.log(secondshop);
-console.log(thirdshop);
-console.log(forthshop);
 
-CookieShop.prototype.table=function(){
-let table = document.getElementById('table');
-let table = document.createElement('table');
-let trow = [];
-let cookiesSold = 0; 
-let totalCookiesSold = 0;
-let head = document.createElement('header');
- trow = document.createElement('trow');
-head.innerText = this.location;
-trow.appendChild(head);
-<<<<<<< HEAD
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
-=======
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
-}
 
-for (let i = 1; i < 5; i++){
-    trow[i] = document.createElement('trow');
-    let cell = document.createElement('cell');
-   cell.innerText = cookiesSold;
-    trow.appendChild(cell);
-    cookiesSold = this.cookiesPerHr();
-    for (let j = 1; j < 5; j++){
-        cell[j]=document.createElement('cell');
-        footer.innerText = CookiesSold ;
-        let footer = document.createElement('footer');
-   footer.innerText = totalCookiesSold ;
-    cell.appendChild(footer);
-    totalCookiesSold += cookiesSold;
-cookieshopTable.appendChild(trow);
-    }           
-    table.appendChild(trow[i]);
-}
-tablearea.appendChild(table);
-cookieshopTable.innerHTML = '';
 
-totalCookiesPerHour();
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-function  totalcookieshops() {
-  for(let  k  in  totalcookieshops) {
-    totalcookieshops[k].render();
+function header(){
+  let trow=document.createElement('tr');
+  let th =document.createElement('th');
+  th.innerText='location';
+  trow.appendChild(th);
+  for (let i=0;i<salesHour.length;i++){
+    th =document.createElement('th');
+    th.innerText=saleshoure[i];
+    trow.appendChild(th);
   }
+  th=docment.createElement('th');
+  th.innerText='totalforalllocations';
+  trow.appendChild(th);
+  trow.appendChild(table);
 }
-// code to add new shop 
-function salmonCookieShop(shop) {
-=======
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
-=======
->>>>>>> ff3b948d33ab4bac89095b795c5d61d1cb4ae2bf
 
-  
+    function  footer(){
+      if(footer.length===0){
+        let tablefooter =document.createElement('tf');
+        let newRow = document.createElement('tfoot');
+      let footerhead=document.createElement('th');
+      footerhead.innerText='total all location';
+      footerhead.appendChild(footerrow);
+      tablefooter.appendChild(cookieTable);
+      
+    let sumoftotal=0;
+    for (let i=0;i<salesHour.length;i++){
+      for (let j=0;j<alllocationshops.length;j++){
+        sumoftotal+=cookiesSoldPerHr[i].alllocationshops[j];
+
+      }
+      footerhead=document.createElement('th');
+      footerhead.innerText=totalCookiesSold ;
+      footerrow.appendChild(footerhead);
+    }
+    footerhead=document.createElement('th');
+    footerhead.innerText=sumoftotal ;
+    footerrow.appendChild(footerhead);     
+  table.appendChild(footerrow);
+
+  (function  cookietable(){
+    header();
+    for (let i=0;i<salesHour.length;i++){
+      alllocationshops.render();
+    }
+    footer();
+  }) ();
